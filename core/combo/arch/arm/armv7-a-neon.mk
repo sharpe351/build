a25b79307098282a228a215668aa1e39c1177aa3
Chet Kener <Cl3Kener@gmail.com>
2014-12-13 09:37:17 -0500

    Cleanup armv7a neon
    * Why don't we have cortex-a9 option and why is the fix cortex a8 being applied to all?
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -21,6 +21,7 @@ ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_ldflags := -Wl,--fix-cortex-a8 
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7
@@ -34,6 +35,3 @@ endif
 arch_variant_cflags += \
     -mfloat-abi=softfp \
     -mfpu=neon
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
