6d7b2de9e6e5ec7b5396fcd3bf72b2daee82ee23
William Casey SEdlacek <w@sedlaceks.org>
2014-12-26 21:15:13 -0800

    Fixing things

--- core/combo/HOST_linux-x86_64.mk
+++ core/combo/HOST_linux-x86_64.mk
@@ -18,11 +18,7 @@
 # Included by combo/select.mk
 
 ifeq ($(strip $(HOST_TOOLCHAIN_PREFIX)),)
-<<<<<<< HEAD
-ifeq ($(USE_HOST_4_8),yes)
-=======
 ifeq ($(USE_HOST_4_8),true)
->>>>>>> 4e8e7dc... Introduce USE_HOST_4_8 Flag
 HOST_TOOLCHAIN_PREFIX := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.15-4.8/bin/x86_64-linux-
 else
 HOST_TOOLCHAIN_PREFIX := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.11-4.6/bin/x86_64-linux-
@@ -36,11 +32,7 @@ HOST_AR  := $(HOST_TOOLCHAIN_PREFIX)ar
 endif # $(HOST_TOOLCHAIN_PREFIX)gcc exists
 
 # gcc location for clang; to be updated when clang is updated
-<<<<<<< HEAD
-ifeq ($(USE_HOST_4_8),yes)
-=======
 ifeq ($(USE_HOST_4_8),true)
->>>>>>> 4e8e7dc... Introduce USE_HOST_4_8 Flag
 HOST_TOOLCHAIN_FOR_CLANG := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.15-4.8/
 else
 HOST_TOOLCHAIN_FOR_CLANG := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.11-4.6/
